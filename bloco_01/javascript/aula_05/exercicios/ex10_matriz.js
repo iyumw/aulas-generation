14e71a1e7909ab5ae7c9f6cb8d8f87c4d98fe6a0
Isis Yume Okamoto <isis.yume2508@gmail.com>
2024-12-06 20:55:33 -0300

    exercicio matriz - aula 05

---
+++ bloco_01/javascript/aula_05/exercicios/ex10_matriz.js
@@ -0,0 +1,24 @@
+const leia = require('readline-sync');
+
+let medias = []
+
+// Qtd de linhas
+let matriz = new Array(9);
+
+// Qtd de colunas 
+for (let i = 0; i < matriz.length; i++) {
+    matriz[i] = new Array(4);
+}
+
+// Preenche a matriz com valores
+for (let iLinha = 0; iLinha < matriz.length; iLinha++){
+    let somaLinha = 0
+    for(let iColuna = 0; iColuna < matriz[iLinha].length; iColuna++){
+        matriz[iLinha] [iColuna] = leia.questionFloat("\nDigite um numero: ");
+        somaLinha += matriz[iLinha][iColuna]
+    }
+    medias[iLinha] = ((somaLinha / matriz[iLinha].length).toFixed(1))
+} 
+
+console.table(matriz)
+console.log(`\nAs médias de cada linha são: [${medias.join(", ")}]`);
